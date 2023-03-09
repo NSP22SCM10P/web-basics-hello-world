08662e1fbe785248f0446ab0a70510cad19c84e6
NSP22SCM10P <89713330+NSP22SCM10P@users.noreply.github.com>
2023-03-09 14:02:42 -0500

    //Fixed the port issue

--- server.js
+++ server.js
@@ -5,11 +5,31 @@ const express = require('express');
 // Constants
 const PORT = 9090;
 const HOST = '0.0.0.0';
-
+//document.body.style.background = "blue";
 // App
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  const html = `
+    <html>
+      <head>
+        <style>
+          body {
+            background-color: #e6e6e6;
+          }
+          img {
+            max-width: 100%;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+        <p>Welcome to my website!</p>
+        <img src="web-basics-hello-world/example.jpg" alt="My Image">
+      </body>
+    </html>
+  `;
+  res.send(html);
 });
 
 app.listen(PORT, HOST, () => {
